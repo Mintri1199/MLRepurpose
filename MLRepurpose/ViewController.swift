416457b24c67cffa7c55d3b0dfbec9fe4bb69f18
Jackson Ho <mintri1199@gmail.com>
2020-01-07 02:07:25 -0800

    [Initial]

---
+++ MLRepurpose/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MLRepurpose
+//
+//  Created by Jackson Ho on 1/7/20.
+//  Copyright © 2020 Jackson Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MLRepurpose' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MLRepurpose
+
+  target 'MLRepurposeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
